cee1b37a639b0564a2ba9b397ffab4f2cf658604
sid365365 <anubis105@sibnet.ru>
2015-11-30 15:01:45 +0700

    art1

---
+++ art/Figure.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace art
+{
+    public enum Colors
+    {
+        Red,
+        Black,
+        Blue,
+        Green
+    }
+
+
+    public class Line
+    {
+        public int positionX;
+        public int positionY;
+        public int positionX2;
+        public int positionY2;
+
+        public enum ParametrsLine
+        {
+            positionX,
+            positionY,
+            positionX2,
+            positionY2
+        }
+
+        public int ItLineInRectangle(List<Line> lines, Rectangle rectangle)
+        {
+            foreach (Line line in lines)
+            {
+                var lineX = line.positionX;
+                var lineY = line.positionY;
+                var lineX2 = line.positionX2;
+                var lineY2 = line.positionY2;
+                var recX = rectangle.positionX;
+                var recY = rectangle.positionY;
+                var recXX = recX + rectangle.heightRectangle;
+                var recYY = recY + rectangle.widthRectangle;
+                if (lineX >= recX & lineX <= recXX)
+                {
+                    if (lineY >= recY & lineY <= recYY)
+                    {
+                        return 1;
+                    }
+                }
+
+                if (lineX2 >= recX & lineX2 <= recXX)
+                {
+                    if (lineY2 >= recY & lineY2 <= recYY)
+                    {
+                        return 2;
+                    }
+                }
+            }
+            return 0;
+        }
+
+
+        public void ChekLine()
+        {
+        }
+
+        public void LineDrow(Graphics g, Int32 x1, Int32 y1, Int32 x2, Int32 y2, Color color)
+        {
+            Pen pen = new Pen(color);
+            g.DrawLine(pen, x1, y1, x2, y2);
+            positionX = x1;
+            positionY = y1;
+            positionX2 = x2;
+            positionY2 = y2;
+        }
+    }
+
+    public class UpdatePictur
+    {
+        public static void Update(List<Line> lines, List<Rectangle> rectangles, Graphics g)
+        {
+            g.Clear(Color.White);
+            if (rectangles.Count > 0)
+                foreach (Rectangle rectangle in rectangles)
+                {
+                    rectangle.rectangleDrow(g, Convert.ToInt32(rectangle.positionX),
+                        Convert.ToInt32(rectangle.positionY), Convert.ToInt32(rectangle.widthRectangle),
+                        Convert.ToInt32(rectangle.heightRectangle), Color.Black);
+                }
+            if (lines.Count > 0)
+                foreach (Line line in lines)
+                {
+                    line.LineDrow(g, line.positionX, line.positionY, line.positionX2, line.positionY2, Color.Black);
+                }
+        }
+    }
+
+    public class Rectangle
+    {
+        public int positionX;
+        public int positionY;
+        public int widthRectangle;
+        public int heightRectangle;
+
+        public void rectangleDrow(Graphics g, Int32 x1, Int32 y1, Int32 widthRectangl, Int32 heightRectangl, Color color)
+        {
+            Pen pen = new Pen(color);
+            g.DrawRectangle(pen, x1, y1, widthRectangl, heightRectangl);
+            positionX = x1;
+            positionY = y1;
+            widthRectangle = widthRectangl;
+            heightRectangle = heightRectangl;
+        }
+
+        public bool rectenglChek(int xx1, int yy1)
+        {
+            if (positionX < xx1 & xx1 < positionX + widthRectangle)
+            {
+                if (positionY < yy1 & yy1 < positionY + heightRectangle)
+                {
+                    //MessageBox.Show("тут");
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ art/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace art
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(191, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(2, 68);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(57, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Линия";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(2, 91);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(105, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Прямоугольник";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(2, 114);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(185, 17);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Перемещение прямоугольника";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(191, 515);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(232, 515);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(319, 515);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(278, 515);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(503, 515);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(544, 515);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 534);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
